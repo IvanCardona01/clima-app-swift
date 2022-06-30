13d89b69cd405d870e1a3b6f75fbce01d156d6e9
Carlos Rios <carlosrios@Carloss-Mac-mini.local>
2022-06-30 18:57:22 -0500

    template-clima-app-with-delegate-pattern

---
+++ Clima/Controller/DelegatePattern.swift
@@ -0,0 +1,14 @@
+//
+//  DelegatePattern.swift
+//  Clima
+//
+//  Created by Carlos Rios on 30/06/22.
+//  Copyright © 2022 App Brewery. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class DelegatePattern {
+     
+}
